323fd623e384ca6c7bdba922528576b0f6da1e92
Tyler <realpsych22@gmail.com>
2016-07-15 18:56:26 +0000

    Yay!

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get("/", function(req, res){
-    res.send("hello world");
+    var ip = req.headers["x-forwarded-for"];
+    var lang = req.headers["accept-language"].split(",")[0];
+    var soft = req.headers["user-agent"].split("(")[1].split(")")[0];
+    
+    res.send({
+        ipaddress: ip, language: lang, software: soft
+    });
 });
 
 
